0bb8be61d9910598d219d7c47abe5fdd62b93ce3
Antonio <toni.games2101@gmail.com>
2019-12-11 10:22:09 +0100

    Reg3 no funciona
    Existe un error al introducir los ingredientes de las pizzas, hacer reg2 en casa

---
+++ Cosas Hechas/Registros/Reg3.c
@@ -0,0 +1,110 @@
+#include <iostream>
+
+using namespace std;
+
+typedef char TCadena[15];
+
+typedef struct{
+  int hora;
+  int minutos;
+}THora;
+
+typedef struct{
+  int num;
+  TCadena nombre;
+  char ing[3];
+  int numIngred;
+  bool enviado;
+  THora hora;
+}TPedido;
+
+typedef TPedido TPizzeria[100];
+
+char menu();//Lo pone como char por que lo trata como char, podria ser int
+void alta(TPizzeria, int &);//contador de pizzas, lleva & por que se irá sumando uno importante ponerlo por referencia
+void marcar(TPizzeria, int);//Para saber cuantas celdas tienen pedidos
+void mostrar(TPizzeria, int);
+
+int main(){
+  TPizzeria pizzas;
+  int cont;//Para saber cuantas pizzas tienes
+  char op;//Será la opcion.
+  cont = 0;
+
+  op = menu();
+  switch (op) {
+    case '1':alta(pizzas, cont);
+            break;
+    case '2':marcar(pizzas, cont);
+            break;
+    case '3':mostrar(pizzas, cont);
+            break;
+  }while (op!='4');
+return 0;
+}
+
+
+char menu(){
+  char op;
+  do {
+    cout<<" 1.-Dar de alta un pedido"<<endl;
+    cout<<" 2.-Marcar un pedido como enviado"<<endl;
+    cout<<" 3.-Mostrar el número de veces que hay un ingrediente"<<endl;
+    cout<<" 4.-Salir del programa"<<endl;
+    cout<<" Has elegido: ";
+    cin>>op;
+  } while(op<'1'|| op>'4');
+  return op;
+}
+
+void alta(TPizzeria pizzas, int &cont){
+  if (cont== 100) {
+    cout << "Error, no se pueden dar de alta más pedidos"<<endl;
+  }
+  else{
+    pizzas[cont].num=cont+1;
+    cout<<"Pedido número "<< cont+1 <<endl;
+    cout <<"Pizza: ";
+    cin.get();//Para limpiar el buffer, sino el cin.getline da problemas.
+    cin.getline(pizzas[cont].nombre, 15);
+    cout<<"Número de extras: ";
+    cin >> pizzas[cont].numIngred;
+    for (int i = 0; i < pizzas[cont].numIngred; i++) {
+      cout << "Ingrediente "<< i+1 << " ";
+      cin >>pizzas[cont].ing[i];
+    }
+    pizzas[cont].enviado=false;
+    pizzas[cont].hora.hora=0;
+    pizzas[cont].hora.minutos=0;
+    cont ++;
+  }
+}
+
+void marcar(TPizzeria pizzas, int cont){
+  int num;
+  cout <<"Número de pedido ";
+  cin >>num;
+  if (num<=cont) {
+    pizzas[num-1].enviado=true;
+    cout << "Pedido "<< num <<" enviado con exito"<<endl;
+  }
+  else{
+    cout<< "Error, introduzca un pedido valido"<<endl;
+  }
+}
+
+void mostrar(TPizzeria pizzas, int cont){
+  char ing;
+  int i, j, cantidad;
+  cantidad = 0;
+  cout << "Ingrediente a contar: ";
+  cin >> ing;
+  for (i = 0; i < cont; i++) {
+    for (j = 0; j < pizzas[i].numIngred; j++) {
+      if (ing = pizzas[i].ing[j]) {
+        cantidad ++;
+      }
+    }
+  }
+  cout<< "Tienes que preparar "<< cantidad << " unidades del ingrediente "<<ing<<endl;
+}
